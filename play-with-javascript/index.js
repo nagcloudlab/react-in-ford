35da15b02728e7a95f591c6f3b8d8bc0ede11dd1
Nag <nagcloudlab@gmail.com>
2024-03-26 12:39:02 +0530

    day 2 commit

--- play-with-javascript/index.js
+++ play-with-javascript/index.js
@@ -1,39 +1,16 @@
-"use strict";
+// e.g
 
-/*
+async function fetchTodos() {
+  const response = await fetch(
+    "https://jsonplaceholder.typicode.com/todos?_limit=5"
+  );
+  const todos = await response.json();
+  console.log(todos);
+}
 
-    function closure
+function renderTodos() {
+  fetchTodos();
+  console.log("loading todos");
+}
 
-    A closure is a function having access to the parent scope, 
-    even after the parent function has closed.
-
-
-    when/why to use closure?
-
-    1. To abstract public behavior of any module
-    2. While execuuating async code
-
-
-*/
-
-//  author : Nag
-//  module : counter
-
-// self executing function
-// IIFE - Immediately Invoked Function Expression
-
-const counter = (function init() {
-  let count = 0; // private
-  function increment() {
-    // public
-    count++;
-  }
-  function getCount() {
-    // public
-    return count;
-  }
-  return {
-    increment,
-    getCount,
-  };
-})();
+renderTodos();
