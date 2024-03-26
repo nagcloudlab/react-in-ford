e173fd774017d7854b0c39e1969ddff30869df52
Nag <nagcloudlab@gmail.com>
2024-03-26 13:04:11 +0530

    exercise

--- play-with-javascript/index.js
+++ play-with-javascript/index.js
@@ -1,16 +1,92 @@
-// e.g
+// 1. addTodo
+// 2. deleteTodo
+// 3. editTodo
+// 4. toggleTodo ( i.e. mark as completed or not)
+// 5. toggleAllTodos ( i.e. mark all as completed or not)
+// 6. clearAllCompletedTodos
+// 7. displayTodos ( filter by all, active, completed)
 
-async function fetchTodos() {
-  const response = await fetch(
-    "https://jsonplaceholder.typicode.com/todos?_limit=5"
-  );
-  const todos = await response.json();
-  console.log(todos);
-}
+todos = [
+  { id: 1, title: "Learn JavaScript", completed: false },
+  { id: 2, title: "Learn React", completed: false },
+  { id: 3, title: "Learn Node", completed: false },
+  { id: 4, title: "Learn Express", completed: false },
+];
 
-function renderTodos() {
-  fetchTodos();
-  console.log("loading todos");
-}
+const service = {
+  addTodo(title) {
+    const newTodo = {
+      id: todos.length + 1,
+      title,
+      completed: false,
+    };
+    // mutable
+    // todos.push(newTodo); // mutable
+    // // immutable
+    // todos = todos.concat(newTodo);
+    todos = [...todos, newTodo];
+  },
+  deleteTodo(id) {
+    // mutable
+    // const index = todos.findIndex((todo) => todo.id === id);
+    // todos.splice(index, 1);
+    // immutable
+    todos = todos.filter((todo) => todo.id !== id);
+  },
+  editTodo(id, title) {
+    // mutable
+    // const index = todos.findIndex((todo) => todo.id === id);
+    // todos[index].title = title;
 
-renderTodos();
+    // immutable
+    todos = todos.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, title };
+      }
+      return todo;
+    });
+  },
+  toggleTodo(id) {
+    // mutable
+    // const index = todos.findIndex((todo) => todo.id === id);
+    // todos[index].completed = !todos[index].completed;
+
+    // immutable
+    todos = todos.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, completed: !todo.completed };
+      }
+      return todo;
+    });
+  },
+  toggleAllTodos() {
+    // mutable
+    // const isAllCompleted = todos.every(todo => todo.completed);
+    // todos.forEach(todo => todo.completed = !isAllCompleted);
+
+    // immutable
+    const areAllCompleted = todos.every((todo) => todo.completed);
+    todos = todos.map((todo) => ({
+      ...todo,
+      completed: !areAllCompleted,
+    }));
+  },
+  clearAllCompletedTodos() {
+    // mutable
+    // todos = todos.filter(todo => !todo.completed);
+    // immutable
+    todos = todos.filter((todo) => !todo.completed);
+  },
+  displayTodos(filter = "all") {
+    switch (filter) {
+      case "all":
+        return todos;
+      case "active":
+        return todos.filter((todo) => !todo.completed);
+      case "completed":
+        return todos.filter((todo) => todo.completed);
+      default:
+        return todos;
+    }
+  },
+};
